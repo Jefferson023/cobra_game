94ad9683ebcf6486b3b570eca2cda5be414fc5a5
jefferson <jeffersonxrhudson@gmail.com>
2020-10-18 05:16:19 -0300

    labels adicionados, faltando apenas o sistema de colisão e refatorar

--- scripts/game.gd
+++ scripts/game.gd
@@ -4,13 +4,22 @@ const food_scene = preload("res://food.tscn")
 var food = null
 var rng = RandomNumberGenerator.new()
 signal grow_snake
+var actual_score = 0
+var last_score = 0
 
 func _ready():
+	$score.text = "score: " + str(actual_score)
 	erase_and_show_food()
-	
+
+func _process(delta):
+	if (Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_right")
+	or Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("ui_down")):
+		if ($welcome_msg != null):
+			$welcome_msg.queue_free()
+		
 func erase_and_show_food():
 	if (food != null):
-		food.free()
+		food.queue_free()
 	food = food_scene.instance()
 	food.position = select_random_position_food()
 	add_child(food)
@@ -25,8 +34,10 @@ func select_random_position_food():
 
 func _on_head_area_shape_entered(area_id, area, area_shape, self_shape):
 	if (food.get_instance_id() == area_id):
-		erase_and_show_food()
-		#aumentar os pontos
 		emit_signal("grow_snake")
+		erase_and_show_food()
+		actual_score += 1
+		$score.text = "score: " + str(actual_score)
 	else:
-		print("colidiu com o corpo")	
+		pass
+		
